debf3801493d042922704bf2f3a869844a165f89
Tim Dubath <tim.dubath@lernende.bbw.ch>
2020-01-10 14:54:06 +0100

    initial commit

---
+++ src/main/java/ch/bbw/td/consolecalculator/App.java
@@ -0,0 +1,30 @@
+package ch.bbw.td.consolecalculator;
+
+import java.util.Scanner;
+
+public class App {
+	
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		int valueA;
+		int valueB;
+		
+		System.out.println("Console Calculator");
+		System.out.println("==================");
+		System.out.println();
+		
+		System.out.println("Bitte erste und danach zweite Zahl eingeben");
+		
+		Scanner numberOne = new Scanner(System.in);
+		
+		
+		
+		Scanner numberTwo = new Scanner(System.in);
+		
+		valueA = numberOne.nextInt();
+		valueB = numberTwo.nextInt();
+		
+		System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
+	}
+
+}
